3ae4b87e122623e56ea54cd43ef171cb236888db
Isira-Ratnayake <81465273+Isira-Ratnayake@users.noreply.github.com>
2022-11-13 20:54:36 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using HospitalCRM.Service;
+using HospitalCRM.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using HospitalCRM.View;
+
+namespace HospitalCRM
+{
+    
+    internal static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new LoginForm());
+        }  
+    }
+    
+    /*
+    internal static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            CrmEngine ce = new CrmEngine();
+            ce.Login("receptionist@gmail.com", "pass@receptionist");
+            Patient p = new Patient(7, false);
+            ce.GetPatient(p);
+            Console.Out.WriteLine(p.getPatientDob());
+        }
+    }
+    */
+
+}
